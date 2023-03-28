b566e435dc505b59e723be4c2c59e01178fe08f6
bluebaby666 <nuist-2-7@163.com>
2023-03-28 21:03:56 +0800

    提交代码

---
+++ test3_25/test3_25/test3_25_1.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
+//int main()
+//{
+//	int a = 10;
+//	int* p=&a;
+//	*p = 20;
+//	printf("a=%d\n", a);
+//	/*printf("%p\n", &a);
+//	printf("%p\n", p);*/
+//	//printf("%p\n", &a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	/*printf("%p\n", &a);*/
+//	int* p = &a;
+//	*p = 520;
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+//struct Book
+//{
+//	char name[20];
+//	short price;
+//};
+//int main()
+//{
+//	struct Book b1 = { "爱的誓言",520 };
+//	struct Book* pc = &b1;
+//	printf("%s\n", pc->name);
+//	printf("%d\n", pc->price);
+//	/*printf("书名：%s\n", b1.name);
+//	printf("价格：%d\n", b1.price);
+//	b1.price = 1314;
+//	printf("修改后的价格：%d\n", b1.price);*/
+//	return 0;
+//}
